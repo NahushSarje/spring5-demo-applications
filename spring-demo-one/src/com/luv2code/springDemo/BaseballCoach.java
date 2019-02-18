efca6899e276a7ce7accad5a274af072a577eb0c
NahushSarje <sarje_nahush@yahoo.co.in>
2019-02-18 23:31:40 +0530

    Add files via upload

---
+++ spring-demo-one/src/com/luv2code/springDemo/BaseballCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springDemo;
+
+public class BaseballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Spend 40 min of practive with ball daily.";
+	}
+}
---
+++ spring-demo-one/src/com/luv2code/springDemo/MyApp.java
@@ -0,0 +1,14 @@
+package com.luv2code.springDemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		//Create an object
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		//use object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
